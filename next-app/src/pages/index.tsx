ae4d3702e968536654afdd94344cc00ba0b88229
Mina <chirusmina@gmail.com>
2024-04-03 11:23:50 +0300

    Add MUI theming and update app layout

--- next-app/src/pages/index.tsx
+++ next-app/src/pages/index.tsx
@@ -1,12 +1,13 @@
 import Head from "next/head";
 import Image from "next/image";
 import MyAppbar from "../../components/MyAppbar";
-
-
+import { Container, Grid, useTheme } from "@mui/material";
 
 export default function Home() {
-
+  const theme = useTheme();
   return (
-    <MyAppbar/>
+    <Grid sx={{backgroundColor: theme.palette.background.default}}>
+      <MyAppbar/>
+    </Grid>
   );
 }
