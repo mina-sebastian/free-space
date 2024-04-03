1d269955ae46ff41406fd9a05ad6cc5b28b85c24
Mina <chirusmina@gmail.com>
2024-04-03 17:14:40 +0300

    Add user and test API endpoints

--- next-app/src/pages/api/addUser.ts
+++ next-app/src/pages/api/addUser.ts
@@ -7,18 +7,27 @@ const prisma = new PrismaClient()
 type ResponseData = {
   message: string
 }
- 
+// EXAMPLE: http://localhost:3000/api/addUser
+// This will add a user to the database and return a message
+// if the user was added successfully
+// or an error message if the user was not added
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
-    // Create a new user with a new post
-    let user = await prisma.user.create({
-        data: {
-          name: 'Alice',
-          email: '',
-        },
+    try{
+      let user = await prisma.user.create({
+          data: {
+            name: 'Alice',
+            email: 'alice@s.unibuc.ro',
+          },
+        }
+      )
+      return res.status(200).json({ message: "Added: "+user.email + ' ' + user.name})
+    }catch(e){
+      console.log(e)
+      return res.status(500).json({message: "Error: "+e.message})
     }
-    )
-    res.status(200).json({ message: user.toString()})
+    
+    
 }
