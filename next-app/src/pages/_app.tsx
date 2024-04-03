a5b5c933cf4b1b666983986ff62ed005c31bd5e2
Mina <chirusmina@gmail.com>
2024-04-03 11:23:50 +0300

    Add MUI theming and update app layout

--- next-app/src/pages/_app.tsx
+++ next-app/src/pages/_app.tsx
@@ -1,6 +1,41 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
+import { ThemeProvider, createTheme, responsiveFontSizes } from '@mui/material/styles';
+import { green, purple } from '@mui/material/colors';
+import { CssBaseline } from "@mui/material";
+
+const theme = responsiveFontSizes(createTheme({
+  
+  palette: {
+    mode: 'dark',
+    background: {
+      default: '#13093c',
+      paper: '#1b074f',
+    },
+    
+  },
+  components: {
+    MuiAppBar: {
+      styleOverrides: {
+        root: {
+          borderBottomLeftRadius: 30,
+          borderBottomRightRadius: 30,
+        },
+      },
+    },  
+  },
+  
+  shape: {
+    borderRadius: 15,
+  },
+}));
+
+
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline enableColorScheme />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
