e2ac2af4eb5bb3e6df0b6bb3e1335a23ff1d6b7c
Mina <chirusmina@gmail.com>
2024-04-03 17:14:40 +0300

    Add user and test API endpoints

--- next-app/src/pages/api/test.ts
+++ next-app/src/pages/api/test.ts
@@ -7,16 +7,15 @@ const prisma = new PrismaClient()
 type ResponseData = {
   message: string
 }
- 
+
+// EXAMPLE: http://localhost:3000/api/test
+// This will return a list of users from the database
+// if there are no users in the database it will return an empty string
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
     
-    let user = await prisma.user.findMany({
-      include:{
-        posts: true
-      }
-    });
-  res.status(200).json({ message: user.map((u) => u.name).join(', ')})
+    let user = await prisma.user.findMany();
+    res.status(200).json({ message: user.map((u) => `${u.email} ${u.name}`).join(', ')})
 }
