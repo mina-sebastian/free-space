db232a9a73fc19b495987c4006de1a01caa8b575
Yanis3Pique <yanispavel.popescu@gmail.com>
2024-04-08 11:52:40 +0300

    Update User model and add Session, Account, Folder, File, and Link models

--- next-app/src/pages/index.tsx
+++ next-app/src/pages/index.tsx
@@ -2,6 +2,8 @@ import Head from "next/head";
 import Image from "next/image";
 import MyAppbar from "../../components/MyAppbar";
 import { Container, Grid, useTheme } from "@mui/material";
+import * as React from 'react';
+import Box from '@mui/material/Box';
 
 export default function Home() {
   const theme = useTheme();
@@ -9,6 +11,16 @@ export default function Home() {
   return (
     <Grid sx={{backgroundColor: theme.palette.background.default}}>
       <MyAppbar/>
+
+      <Box component="section" sx={{ p: 25, border: '1px transparent white' }}>
+        Basic instructions for the user:
+        <Box component="section" sx={{ p: 1, border: '1px transparent white' }}>
+            1. Login/Logout
+        </Box>
+        <Box component="section" sx={{ p: 1, border: '1px transparent white' }}>
+            2. Upload Files
+        </Box>
+      </Box>
     </Grid>
   );
 }
