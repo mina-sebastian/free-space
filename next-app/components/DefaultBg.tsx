e910780e9dea83ee6f297deb80c3e1b8e5d1b9b9
Mina <chirusmina@gmail.com>
2024-04-10 10:11:22 +0300

    Update Docker Compose file and add new volumes for next-app components

---
+++ next-app/components/DefaultBg.tsx
@@ -0,0 +1,27 @@
+
+import MyAppbar from "./MyAppbar";
+import { Grid, useTheme } from "@mui/material";
+import * as React from 'react';
+
+export default function DefaultBg({ children }) {
+  const theme = useTheme();
+  
+  return (
+    <Grid sx={{backgroundColor: theme.palette.background.default}}>
+      <MyAppbar/>
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        sx = {{
+          paddingTop: 12,
+          minHeight: '100vh'
+      }}>
+        <Grid width="100%" maxWidth="1500px" alignItems="center">
+            {children}
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+}
