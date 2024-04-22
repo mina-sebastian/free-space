fb13580c81eaab58aaf3dc49c0ad6106548d9358
Mina <chirusmina@gmail.com>
2024-04-22 23:42:32 +0300

    Update .gitignore and add tusd/data/* to ignore list

--- next-app/components/UploadModal.tsx
+++ next-app/components/UploadModal.tsx
@@ -1,32 +1,22 @@
-import { useEffect, useState } from "react";
-import Uppy from "@uppy/core";
-import Dashboard from '@uppy/dashboard';
-import Tus from '@uppy/tus';
+import React, { useEffect, useState } from'react'
+import Uppy from'@uppy/core'
+import Tus from'@uppy/tus'
+import { Dashboard, DashboardModal, DragDrop, ProgressBar, FileInput } from '@uppy/react'
 
-import '@uppy/core/dist/style.min.css';
-import '@uppy/dashboard/dist/style.min.css';
+import '@uppy/core/dist/style.css'
+import '@uppy/dashboard/dist/style.css'
+import '@uppy/drag-drop/dist/style.css'
+import '@uppy/file-input/dist/style.css'
+import '@uppy/progress-bar/dist/style.css'
+import { Button } from '@mui/material'
+import { useSession } from 'next-auth/react'
 
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
-
-const style = {
-  position: 'absolute' as 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-};
 
 export default function UploadModal() {
 
-    const [uppy, setUppy] = useState<Uppy>();
+    const { data: session } = useSession();
+
+    const [uppy, setUppy] = useState<Uppy>(null);
 
     const [open, setOpen] = React.useState(false);
     const handleOpen = () => setOpen(true);
@@ -34,29 +24,31 @@ export default function UploadModal() {
 
     useEffect(() => {
     if (typeof window !== 'undefined') { // Ensures this code block runs only on the client
-        const up = new Uppy()
-        .use(Dashboard, { inline: true, target: '#uppy-dashboard' }) // Target must match a div ID in your component
-        .use(Tus, { endpoint: 'http://localhost/files/' });
+      if(!!session && !!session.user){
+        const up = new Uppy({
+          meta: { tkn: session.user.id},
+        })
+            .use(Tus,
+              {
+                endpoint: 'http://localhost/files/'
+              })
+            ;
         setUppy(up);
-
-        // Cleanup function to close Uppy on component unmount
+        
         return () => up.close();
+      }
     }
-    }, []);
+    }, [session]);
   
   return (
     <>
     <Button onClick={handleOpen}>Open modal</Button>
-      <Modal
+    {!!uppy && <DashboardModal
+        uppy={uppy}
         open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-            <div id="uppy-dashboard"></div>
-        </Box>
-      </Modal>
+        // target={document.body}
+        onRequestClose={handleClose}
+    />}
     
  
     </>
--- next-app/src/pages/api/cloud/hooks.ts
+++ next-app/src/pages/api/cloud/hooks.ts
@@ -1,9 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import prisma from '../../../../libs/prismadb'
 
-import { getServerSession } from "next-auth/next"
-import { authOptions } from "../auth/[...nextauth]"
-
 import { createId } from '@paralleldrive/cuid2';
 
 type HookRequest = {
@@ -23,6 +20,7 @@ type HookRequest = {
     ChangeFileInfo?: {
       ID: string;
       MetaData: {
+        tkn: string;
         filename: string;
         creation_time: string;
       };
@@ -45,23 +43,38 @@ export default async function handler(
         Headers: {},
       },
     };
+
     if (req.method === 'POST') {
-        console.log('Received hook request:', req.body);
         const hookRequest: HookRequest = req.body;
     
-        console.log('Received hook request:', hookRequest);
+        // console.log('Received hook request:', hookRequest);
 
         if (hookRequest.Type === 'pre-create') {
           const metaData = hookRequest.Event.Upload.MetaData;
+          // console.log("MetaData: ", metaData);
           const isValid = metaData && 'filename' in metaData;
-          if (!isValid) {
+
+          const user = await prisma.user.findUnique({
+            where: {
+              id: metaData.tkn
+            }
+          });
+
+          if(!user){
+            hookResponse.RejectUpload = true;
+            hookResponse.HTTPResponse.StatusCode = 401;
+            hookResponse.HTTPResponse.Body = 'Not Authorized';
+          }else if (!isValid) {
             hookResponse.RejectUpload = true;
             hookResponse.HTTPResponse.StatusCode = 400;
-            hookResponse.HTTPResponse.Body = 'no filename provided';
+            hookResponse.HTTPResponse.Body = 'No filename provided';
           } else {
+            hookResponse.RejectUpload = false;
+            hookResponse.HTTPResponse.StatusCode = 200;
             hookResponse.ChangeFileInfo = {
               ID: `frspc-${createId()}`,
               MetaData: {
+                tkn: metaData.tkn,
                 filename: metaData.filename,
                 creation_time: new Date().toUTCString(),
               },
@@ -70,8 +83,41 @@ export default async function handler(
         }
     
         if (hookRequest.Type === 'post-finish') {
-          const { ID, Size, Storage } = hookRequest.Event.Upload;
-          console.log(`Upload ${ID} (${Size} bytes) is finished. Find the file at: ${Storage}`);
+          const { ID, Size, MetaData } = hookRequest.Event.Upload;
+          console.log("MetaData: ", MetaData);
+          console.log(`Upload ${ID} (${Size} bytes) is finished.`);
+
+          const user = await prisma.user.findUnique({
+            where: {
+              id: MetaData.tkn
+            }
+          });
+
+          const folders = await prisma.folder.findMany({
+            where: {
+              name: "home",
+              userId: user.id
+            }
+          });
+          let folder = folders[0];
+          if(!folder){
+            folder = await prisma.folder.create({
+              data: {
+                name: "home",
+                userId: user.id
+              }
+            });
+          }
+
+          const file = await prisma.file.create({
+            data: {
+              path: "http://localhost/files/"+ID,
+              name: MetaData.filename,
+              size: Size,
+              userId: user.id,
+              folderId: folder.folderId
+            }
+          });
         }
     
         console.log('Responding with hook response:');
